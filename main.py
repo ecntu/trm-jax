498c6e1f75478cdbe93fc8cc2266b601268646d8
Emilio Cantu <emiliocantu.500@gmail.com>
2025-12-03 18:10:03 +0000

    data parallel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import jax
 import jax.numpy as jnp
 from jax import lax
 from jax.lax import stop_gradient as sg
+from jax.sharding import PartitionSpec as P
 from flax import nnx
 from einops import rearrange, reduce
 
@@ -10,6 +11,7 @@ from optax import sigmoid_binary_cross_entropy as binary_ce
 from optax import softmax_cross_entropy_with_integer_labels as softmax_ce
 
 import random
+import contextlib
 from dataclasses import dataclass
 from functools import partial
 from collections import defaultdict
@@ -247,11 +249,13 @@ def eval_step(model, batch, config, rngs):
     }
 
 
-def evaluate_epoch(model, data_iter, config, rngs):
+def evaluate_epoch(model, data_iter, config, rngs, mesh=None):
     totals = defaultdict(float)
     total_weight = 0.0
 
     for batch in data_iter:
+        if mesh is not None:
+            batch = shard_batch(batch)
         metrics = eval_step(model, batch, config, rngs)
         bs = float(metrics.pop("batch_size"))
         for k, v in metrics.items():
@@ -287,6 +291,14 @@ def model_factory(config, param_dtype, compute_dtype, rngs):
     )
 
     return model
+
+
+def shard_batch(batch):
+    # data parallel sharding
+    return {
+        "inputs": jax.device_put(batch["inputs"], P("data", None)),
+        "labels": jax.device_put(batch["labels"], P("data", None)),
+    }
 
 
 @dataclass(frozen=True)
@@ -327,6 +339,14 @@ if __name__ == "__main__":
     seed = config.seed or random.randint(0, 2**32 - 1)
     rngs = nnx.Rngs(seed)
 
+    num_devices = jax.device_count()
+    if num_devices > 1:
+        mesh = jax.make_mesh((num_devices,), ("data",))
+        nnx.use_eager_sharding(True)
+    else:
+        mesh = None
+    print(f"Using mesh: {mesh}")
+
     train_ds = load_dataset(config.dataset, split="train")
     # train_ds = load_dataset(config.dataset, split=f"train[:{config.batch_size}]") # debug by overfitting to single batch
     val_ds = load_dataset(config.dataset, split="test[:1024]")
@@ -336,72 +356,83 @@ if __name__ == "__main__":
     val_loader = Loader(val_ds, batch_size=config.batch_size, epochs=1)
     test_loader = Loader(test_ds, batch_size=config.batch_size, epochs=1)
 
-    model = model_factory(config, param_dtype, compute_dtype, rngs)
-    n_params = sum(jax.tree.map(jnp.size, jax.tree.leaves(nnx.state(model, nnx.Param))))
-    print(f"No. of parameters: {n_params}")
+    mesh_ctx = jax.set_mesh(mesh) if mesh is not None else contextlib.nullcontext()
 
-    lr_schedule = optax.warmup_constant_schedule(
-        init_value=0.0, peak_value=config.lr, warmup_steps=config.lr_warmup_steps
-    )
+    with mesh_ctx:
+        model = model_factory(config, param_dtype, compute_dtype, rngs)
+        n_params = sum(
+            jax.tree.map(jnp.size, jax.tree.leaves(nnx.state(model, nnx.Param)))
+        )
+        print(f"No. of parameters: {n_params}")
 
-    opt = nnx.Optimizer(
-        model=model,
-        tx=optax.chain(
-            optax.clip_by_global_norm(1.0),
-            optax.adamw(
-                learning_rate=lr_schedule,
-                b1=0.9,
-                b2=0.95,
-                eps=1e-4 if config.half_precision else 1e-8,
-                weight_decay=config.weight_decay,
-                # don't apply weight decay to biases or norm params
-                # mask=lambda params: jax.tree.map(
-                #     lambda p: getattr(p, "ndim", 0) > 1, params
-                # ),
-            ),
-        ),
-        wrt=nnx.Param,
-    )
-
-    ema_model = nnx.clone(model)
-
-    # logging
-    writer = metric_writers.create_default_writer(
-        config.workdir, just_logging=jax.process_index() > 0
-    )
-    writer.write_hparams(vars(config))
-    writer.write_scalars(0, {"hparams/n_params": n_params})
-
-    def _val_callback(step, t):
-        writer.write_scalars(step, evaluate_epoch(ema_model, val_loader, config, rngs))
-
-    hooks = [
-        periodic_actions.ReportProgress(num_train_steps=config.steps, writer=writer),
-        periodic_actions.PeriodicCallback(
-            every_steps=config.val_every,
-            on_steps=[config.steps],
-            callback_fn=_val_callback,
-        ),
-    ]
-    if config.workdir is not None and jax.process_index() == 0:
-        hooks.append(
-            periodic_actions.Profile(num_profile_steps=5, logdir=config.workdir)
+        lr_schedule = optax.warmup_constant_schedule(
+            init_value=0.0, peak_value=config.lr, warmup_steps=config.lr_warmup_steps
         )
 
-    with metric_writers.ensure_flushes(writer):
-        for step, batch in enumerate(train_loader, start=1):
-            model, opt, ema_model, train_metrics = train_step(
-                model, ema_model, opt, batch, config, rngs
+        opt = nnx.Optimizer(
+            model=model,
+            tx=optax.chain(
+                optax.clip_by_global_norm(1.0),
+                optax.adamw(
+                    learning_rate=lr_schedule,
+                    b1=0.9,
+                    b2=0.95,
+                    eps=1e-4 if config.half_precision else 1e-8,
+                    weight_decay=config.weight_decay,
+                    # don't apply weight decay to biases or norm params
+                    # mask=lambda params: jax.tree.map(
+                    #     lambda p: getattr(p, "ndim", 0) > 1, params
+                    # ),
+                ),
+            ),
+            wrt=nnx.Param,
+        )
+
+        ema_model = nnx.clone(model)
+
+        # logging
+        writer = metric_writers.create_default_writer(
+            config.workdir, just_logging=jax.process_index() > 0
+        )
+        writer.write_hparams(vars(config))
+        writer.write_scalars(0, {"hparams/n_params": n_params})
+
+        def _val_callback(step, t):
+            writer.write_scalars(
+                step, evaluate_epoch(ema_model, val_loader, config, rngs, mesh)
             )
-            train_metrics["train/lr"] = lr_schedule(step)
-            writer.write_scalars(step, train_metrics)
 
-            for h in hooks:
-                h(step)
+        hooks = [
+            periodic_actions.ReportProgress(
+                num_train_steps=config.steps, writer=writer
+            ),
+            periodic_actions.PeriodicCallback(
+                every_steps=config.val_every,
+                on_steps=[config.steps],
+                callback_fn=_val_callback,
+            ),
+        ]
+        if config.workdir is not None and jax.process_index() == 0:
+            hooks.append(
+                periodic_actions.Profile(num_profile_steps=5, logdir=config.workdir)
+            )
 
-            if step >= config.steps:
-                break
+        with metric_writers.ensure_flushes(writer):
+            for step, batch in enumerate(train_loader, start=1):
+                if mesh is not None:
+                    batch = shard_batch(batch)
+                model, opt, ema_model, train_metrics = train_step(
+                    model, ema_model, opt, batch, config, rngs
+                )
+                train_metrics["train/lr"] = lr_schedule(step)
+                writer.write_scalars(step, train_metrics)
 
-    # TODO:
-    # - run full experiments
-    # - add checkpointing?
+                for h in hooks:
+                    h(step)
+
+                if step >= config.steps:
+                    break
+
+        # TODO:
+        # - run full experiments
+        # - add checkpointing?
