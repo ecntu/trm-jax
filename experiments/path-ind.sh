6c32241bac0bfefc8b9afae13002ee7b68bccb07
Emilio Cantú <39497904+ecntu@users.noreply.github.com>
2026-03-25 22:51:49 +0000

    skip if tested, prune n widths

--- experiments/path-ind.sh
+++ experiments/path-ind.sh
@@ -29,10 +29,10 @@ for seed in {1..5}; do
             N_WIDTHS=(0)
         elif [[ "$variant" == "rand_N_sup" ]]; then
             T_WIDTHS=(0)
-            N_WIDTHS=(1 2 4 8)
+            N_WIDTHS=(2 4 8)
         elif [[ "$variant" == "rand_T_and_N_sup" ]]; then
             T_WIDTHS=(1 2)
-            N_WIDTHS=(1 2 4 8)
+            N_WIDTHS=(2 4 8)
         else
             T_WIDTHS=(0)
             N_WIDTHS=(0)
@@ -58,11 +58,14 @@ for seed in {1..5}; do
                 WORKDIR_SUFFIX="/wT${t_width}_wN${n_width}"
             fi
 
+            WORKDIR="${LOG_PREFIX}/${variant}${WORKDIR_SUFFIX}/seed${seed}"
+            [[ -f "${WORKDIR}/TESTED" ]] && { echo "Skipping (already tested): ${WORKDIR}"; continue; }
+
             echo "Running with variant=${variant}, t_width=${t_width}, n_width=${n_width}, seed=${seed}, size=${SIZE}"
             uv run --with jax[tpu] main.py \
                 $VARIANT_ARGS $WIDTH_ARGS $INIT_ARGS --seed $seed \
                 --steps $STEPS --test_size 10_000 --N_sup_test $N_SUP_TEST \
-                --max_checkpoints 0 --workdir ${LOG_PREFIX}/${variant}${WORKDIR_SUFFIX}/seed${seed} $EXTRA_ARGS
+                --max_checkpoints 0 --workdir "${WORKDIR}" $EXTRA_ARGS
 
         done
         done
